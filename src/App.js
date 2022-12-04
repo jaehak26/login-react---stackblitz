461dec2120297db6b27da8b587f08750f53ffe11
jaehak26 <68010205+jaehak26@users.noreply.github.com>
2022-12-04 23:13:51 +0900

    3 files modified

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './style.css';
 import LoginForm from './Login/LoginForm';
 import { Button } from '@mui/material';
 import Password from './Login/Password.js';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 
 export default function App() {
+
+  const navigate = useNavigate();
+
+  const navigateToLogin = () => {
+    navigate("/login");
+  };
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <Button>Login</Button>
+      <Button onclick={}>Login</Button>
       <LoginForm></LoginForm>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import App from './App';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import LoginForm from './Login/LoginForm';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
@@ -9,7 +11,12 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <RecoilRoot key="RecoilRoot">
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}></Route>
+          <Route path="/login" element={<LoginForm />}></Route>
+        </Routes>
+      </BrowserRouter>
     </RecoilRoot>
   </StrictMode>
 );
