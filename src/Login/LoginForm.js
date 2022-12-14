b4a01cd55954271c75dee2431f7ddb7516d921f8
jaehak26 <68010205+jaehak26@users.noreply.github.com>
2022-12-14 14:37:24 +0900

    2 files modified

--- src/Login/LoginForm.js
+++ src/Login/LoginForm.js
@@ -4,62 +4,88 @@ import { clickLoginButton } from './ChangeForm';
 import { loginMessage } from '../Recoil/loginState';
 import { useRecoilState } from 'recoil';
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
-import { useAsync } from './useAsync';
+import useAsync from './useAsync';
 
 async function getUser() {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  res = response.json();
-  return res;
+  return response.json();
 }
 
 function LoginForm() {
   const [loginForm, setLoginForm] = useState({ userId: '', userPwd: '' });
   const [loginMessageCoil, setLoginMessageCoil] = useRecoilState(loginMessage);
-  //const [state, refetch] = useAsync(getUser, [], true);
-
-  //const { loading, data: user, error } = state;
+  const [state, refetch] = useAsync(getUser, [], false);
+  const [user, setUser] = useState('');
+  const { loading, data: users, error } = state;
 
   const navigate = useNavigate();
 
   const userExist = () => {
-    if (user[0].userName == loginForm.userId) {
-      setLoginMessageCoil({
-        ...loginMessageCoil,
-        userId: loginForm.userId,
-        message: 'SUCCESS',
-      });
-      navigate('/');
-    } else {
-      alert('잘못된 아이디 혹은 비밀번호입니다.');
-    }
+    refetch();
+    setUser(users[0].username);
+    // if (!users) {
+
+    //   }
   };
 
-  return (
-    <fieldset style={{ maxWidth: '350px' }}>
-      <legend>로그인</legend>
-      <div>
-        <label style={{ margin: '13px' }}>아이디</label>
-        <input
-          name="inputId"
-          onChange={(e) => {
-            setLoginForm({ ...loginForm, userId: e.target.value });
-          }}
-          id="inputId"
-        ></input>
-      </div>
-      <label style={{ margin: '5px' }}>비밀번호</label>
-      <input
-        name="inputPwd"
-        onChange={(e) => {
-          setLoginForm({ ...loginForm, userPwd: e.target.value });
-        }}
-        id="inputPwd"
-      ></input>
+  useEffect(() => {
+    if (user !== '') {
+      console.log(loginForm.userId);
+      if (user == loginForm.userId) {
+        setLoginMessageCoil({
+          ...loginMessageCoil,
+          userId: loginForm.userId,
+          message: 'SUCCESS',
+        });
+        navigate('/');
+      } else {
+        alert('잘못된 아이디 혹은 비밀번호입니다.' + user + loginForm.userId);
+      }
+    }
+  }, [user]);
 
-      <button style={{ margin: '5px' }} onClick={userExist}>
-        login
-      </button>
-    </fieldset>
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!users) return <button onClick={() => refetch()}>불러오기</button>;
+  return (
+    <>
+      <fieldset style={{ maxWidth: '350px' }}>
+        <legend>로그인</legend>
+        <div>
+          <label style={{ margin: '13px' }}>아이디</label>
+          <input
+            name="inputId"
+            onChange={(e) => {
+              setLoginForm({ ...loginForm, userId: e.target.value });
+              console.log(loginForm);
+            }}
+            id="inputId"
+          ></input>
+        </div>
+        <label style={{ margin: '5px' }}>비밀번호</label>
+        <input
+          name="inputPwd"
+          onChange={(e) => {
+            setLoginForm({ ...loginForm, userPwd: e.target.value });
+          }}
+          id="inputPwd"
+        ></input>
+
+        <button style={{ margin: '5px' }} onClick={userExist}>
+          login
+        </button>
+      </fieldset>
+      <>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.username} ({user.name})
+            </li>
+          ))}
+        </ul>
+        <button onClick={refetch}>다시 불러오기</button>
+      </>
+    </>
   );
 }
 
